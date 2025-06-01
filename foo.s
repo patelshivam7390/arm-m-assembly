3445f18ce58b7d4242af0ed703adaf6c38664fa2
shivam patel <97937167+patelshivam7390@users.noreply.github.com>
2025-06-01 13:46:14 +0000

    Tried some more instructions

--- foo.s
+++ foo.s
@@ -8,8 +8,22 @@ vector_table:
 .align 1
 .type reset_handler, %function
 reset_handler:
-    // Infinite loop to prevent returning from main
     mov r1, #1
     mov r2, #2
-    add r3, r1, r2
+    push {r1, r2} // Save registers r1 and r2 on stack
+    mov r2, #0 // Initialize r3 with 10
+    mov r1, #0 // Initialize r3 with 10
+    pop {r1, r2} // Restore registers r1 and r2 from stack
+    ADD r3, r1, r2
+    LSL	r3, r3, #2
+    B shivam
+
+sam:
+    ADD	r4, r3, #4
+    B LOOP
+shivam:
+    SUB	r3, r3, r2
+    B sam
+
+LOOP:
     b .
